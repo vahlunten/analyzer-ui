d713a6976ecb51c5ca31c24117e479c0ff33bcf3
vlado <vchristozov@gmail.com>
2022-10-25 18:39:36 +0200

    tab buttons, tables test

---
+++ src/components/Base.svelte
@@ -0,0 +1,76 @@
+<script context="module" lang="ts">
+    import { path } from 'svelte-pathfinder'
+    import { invalid_attribute_name_character } from 'svelte/internal'
+
+    import Link from '../components/Link.svelte'
+    import { appName } from '../lib/constants';
+    import Table from "./Table.svelte"
+
+    const data = [
+        {
+            label: "CONCLUSION",
+            index: 0,
+        },
+        {
+            label: "MACKA",
+            index: 0
+        },
+        {
+            label: "PES",
+            index: 0
+        },
+        {
+            label: "RAPOTAMO",
+            index: 0
+        }
+    ] 
+</script>
+
+<script lang="ts">
+    // import { bgColor } from '../lib/stores'
+
+
+    // export let title = ''
+
+    // const bgDefault = 'bg-gray-900'
+    // const bgGradient =
+    //     'bg-gradient-to-r from-gray-800 via-green-900 to-gray-800'
+
+    // function toggleBackground() {
+    //     $bgColor = $bgColor === bgDefault ? bgGradient : bgDefault
+    // }
+
+    // $: {
+    //     bgGradient
+    //         .split(' ')
+    //         .forEach((c) =>
+    //             document.body.classList.toggle(c, $bgColor.includes(c)),
+    //         )
+    //     document.body.classList.toggle(bgDefault, $bgColor === bgDefault)
+    // }
+
+</script>
+
+<svelte:head>
+    <!-- <title>{`${title ? title + ' | ' : ''}${appName}`}</title> -->
+</svelte:head>
+
+<main class="w-full">
+    <div class ="m-4 w-full flex overflow-x-auto">
+        <!-- <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">CONCLUSION</button>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button> -->
+
+        <button type="button" class="hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+
+        {#each data as item }
+            <!-- <p>{item.name} - <span>${item.price}</span></p> -->
+            <button id={item.index.toString()} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{item.label}</button>
+        {/each}
+    </div>
+    <Table title={"HTML search results."}/>
+    <Table title={"Different search"}/>
+    <Table title={"Mackoch a pesoch"}/>
+</main>
---
+++ src/components/Table.svelte
@@ -0,0 +1,78 @@
+<script type="module" lang="ts">
+    export let title: string
+
+    import JSONTree from 'svelte-json-tree'
+    // your json data to view
+    const value = {
+        array: [1, 2, 3],
+        bool: true,
+        object: {
+            foo: 'bar',
+        },
+        nested: [
+            {
+                a: [1, '2', null, undefined],
+            },
+        ],
+    }
+</script>
+
+<main class="max-w-full m-4 ">
+    <div class="bg-gray-50 inline-flex rounded-lg">
+        <p
+            class="bg-gray-50 text-md p-6 py-3 width-auto text-xl font-semibold  rounded-lg rounded-b-none dark:text-white"
+        >
+            {title}
+        </p>
+    </div>
+
+    <!-- <p class="bg-gray-50 text-md p-6 width-auto text-xl font-semibold dark:text-white">{title}</p> -->
+    <div class="overflow-x-auto relative shadow-md rounded-tl-none rounded-lg">
+        <table
+            class="w-full text-sm text-left text-gray-700 dark:text-gray-400"
+        >
+            <thead class="text-sm text-gray-700 uppercase bg-gray-50">
+                <tr>
+                    <th scope="col" class="py-3 px-6"> Path </th>
+                    <th scope="col" class="py-3 px-6"> Text found </th>
+                    <th scope="col" class="py-3 px-6"> Text expected </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                    <td class="py-4 px-6"> Sliver </td>
+                    <td class="py-4 px-6"> Laptop </td>
+                    <td class="py-4 px-6"> $2999 </td>
+                </tr>
+                <tr
+                    class="m-4 bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                    <td class="py-4 px-8" colspan="4">
+                        Laptop
+                        <div style="--json-tree-font-size: 14px;">
+                            <JSONTree {value} />
+                        </div>
+
+                        mack
+                    </td>
+                </tr>
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                    <td class="py-4 px-6"> Sliver </td>
+                    <td class="py-4 px-6"> Laptop </td>
+                    <td class="py-4 px-6"> $2999 </td>
+                </tr>
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                    <td class="py-4 px-6"> Sliver </td>
+                    <td class="py-4 px-6"> Laptop </td>
+                    <td class="py-4 px-6"> $2999 </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</main>
